44799d93b33287199d7fd8666a9bc84939300d7d
PawelKuch <kucharski.pawel99@gmail.com>
2024-12-01 23:07:27 +0100

    MenuItem

---
+++ src/main/java/pl/dskimina/foodsy/Entity/MenuItem.java
@@ -0,0 +1,68 @@
+package pl.dskimina.foodsy.Entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "MenuItems")
+public class MenuItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "menu_item_id", length = 512, nullable = false)
+    private String menuItemId;
+
+    @Column(name = "name", nullable = false, length = 512)
+    private String name;
+
+    @Column(name = "description", nullable = true, length = 512)
+    private String description;
+
+    @Column(name = "price", nullable = false)
+    private double price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    private Restaurant restaurant;
+
+    public void setMenuItemId(String menuItemId) {
+        this.menuItemId = menuItemId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public String getMenuItemId() {
+        return menuItemId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+}

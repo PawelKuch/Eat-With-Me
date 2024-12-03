f88401a2dd7042d89248666e1a4e95628d4b62af
PawelKuch <kucharski.pawel99@gmail.com>
2024-12-03 16:34:18 +0100

    Restaurant

--- src/main/java/pl/dskimina/foodsy/controllers/MainController.java
+++ src/main/java/pl/dskimina/foodsy/controllers/MainController.java
@@ -4,15 +4,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.dskimina.foodsy.Service.RestaurantService;
 
 @Controller
 public class MainController {
 
     private static final Logger LOG = LoggerFactory.getLogger(MainController.class);
+    private RestaurantService restaurantService;
+
+    public MainController(RestaurantService restaurantService) {
+        this.restaurantService = restaurantService;
+    }
 
     @GetMapping({"/", "/index"})
     public String index() {
         LOG.debug("Index page");
         return "template";
     }
+
+    @GetMapping("add-restaurant")
+    public String addRestaurant(){
+        return "add-restaurant";
+    }
+
 }

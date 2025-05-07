8ea8b3f9a8a99aa794ec6a7029642031c9750beb
PawelKuch <kucharski.pawel99@gmail.com>
2025-05-07 22:22:56 +0200

    RequestMapping adjustments

--- src/main/java/pl/dskimina/foodsy/controllers/MainController.java
+++ src/main/java/pl/dskimina/foodsy/controllers/MainController.java
@@ -3,19 +3,14 @@ package pl.dskimina.foodsy.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
-import pl.dskimina.foodsy.entity.data.OrderData;
-import pl.dskimina.foodsy.service.OrderService;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
 
     @GetMapping
-    public RedirectView getOrderList(RedirectAttributes ra) {
+    public RedirectView getOrderList() {
         return new RedirectView("/orders");
     }
 }
